9f05c13af40b13e6b09c006a52567ca7cdb7a88c
RomanLatipov <romanlatipov@gmail.com>
2024-08-09 23:52:52 -0400

    create gui window

--- app.py
+++ app.py
@@ -2,4 +2,21 @@ import tkinter as tk
 from tkinter import filedialog
 import os
 
-print("hello world")
+class imageToPdfConverter:
+    def __init__(self, root):
+        self.root = root
+
+        self.initialize_ui()
+
+def initialize_ui(self):
+    title_label = tk.Label(self.root, text="image to pdf converter", font=("Helvetica", 16, "bold"))
+    title_label.pack(pady=10)
+
+def main():
+    root = tk.Tk()
+    root.title("image to pdf")
+    root.geometry("400x600")
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
